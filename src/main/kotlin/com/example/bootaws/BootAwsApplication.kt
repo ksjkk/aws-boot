4ee5fccbdefc29cfadc18fb5fe822cf614004f57
jun.ior <jun.ior@kakaocorp.com>
2022-03-18 14:48:05 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/bootaws/BootAwsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bootaws
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BootAwsApplication
+
+fun main(args: Array<String>) {
+	runApplication<BootAwsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/bootaws/BootAwsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bootaws
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BootAwsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
