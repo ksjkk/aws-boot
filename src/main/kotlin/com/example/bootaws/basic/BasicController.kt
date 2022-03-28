af371e57d2af6e5e72e902984f6e4e7463184d8f
jun.ior <jun.ior@kakaocorp.com>
2022-03-28 15:49:22 +0900

    profile

--- src/main/kotlin/com/example/bootaws/basic/BasicController.kt
+++ src/main/kotlin/com/example/bootaws/basic/BasicController.kt
@@ -1,12 +1,20 @@
 package com.example.bootaws.basic
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
-class BasicController {
+class BasicController(
+    @Value("\${spring.profiles.active}")
+    private val profile: String?,
+
+    @Value("\${server.port}")
+    private val port: String?
+) {
+
 
     @GetMapping(value = ["/ping"])
     fun ping() = "pong".wrapOk()
@@ -14,6 +22,9 @@ class BasicController {
     @GetMapping(value = ["/health"])
     @ResponseBody
     fun healthCheck() = "ok"
+
+    @GetMapping(value = ["/profile"])
+    fun profile() = "profile: $profile // port: $port".wrapOk()
 
     private fun <T> T?.wrapOk() = ResponseEntity.ok(this)
 }
